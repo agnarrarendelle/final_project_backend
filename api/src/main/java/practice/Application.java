48eb85812c401c76ce4d94ef514c82894ac6e81d
agnarrarendelle <danny20001102@gmail.com>
2024-01-09 21:28:30 +0800

    enabled transaction management

--- api/src/main/java/practice/Application.java
+++ api/src/main/java/practice/Application.java
@@ -3,11 +3,12 @@ package practice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 
 @SpringBootApplication
-@EnableWebSocket
 @EnableScheduling
+@EnableTransactionManagement
 public class Application {
 
 	public static void main(String[] args) {
