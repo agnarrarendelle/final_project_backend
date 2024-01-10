2b03ada060310011bd03fbf1f0fd1bcb175988bd
agnarrarendelle <danny20001102@gmail.com>
2024-01-10 19:04:52 +0800

    added a method to fetch a single task with its category

--- repository/src/main/java/practice/repository/TaskRepository.java
+++ repository/src/main/java/practice/repository/TaskRepository.java
@@ -13,4 +13,7 @@ public interface TaskRepository extends JpaRepository<Task, Integer> {
 
     @Query("SELECT t FROM Task t JOIN FETCH t.category WHERE t.groupId = :groupId")
     List<Task> findAllByGroupIdWithCategory(@Param("groupId") Integer groupId);
+
+    @Query("SELECT t FROM Task t JOIN FETCH t.category WHERE t.id = :taskId")
+    Task findByIdWithCategory(@Param("taskId") Integer taskId);
 }
