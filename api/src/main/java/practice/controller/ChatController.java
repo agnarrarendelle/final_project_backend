cfad67bf68a26ab58385c5de914419cbd6474ba4
agnarrarendelle <danny20001102@gmail.com>
2024-01-10 16:57:00 +0800

    added a ws route to broadcast a task

--- api/src/main/java/practice/controller/ChatController.java
+++ api/src/main/java/practice/controller/ChatController.java
@@ -5,8 +5,8 @@ import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import practice.dto.ChatMessageDto;
+import practice.dto.TaskWsNotificationDto;
 import practice.service.ChatService;
 
 @Controller
@@ -19,5 +19,11 @@ public class ChatController {
     public ChatMessageDto sendMessage(ChatMessageDto message, @DestinationVariable("groupId") int groupId){
         chatService.saveNewMessage(groupId, message);
         return message;
+    }
+
+    @MessageMapping("/group/{groupId}/task/{taskId}")
+    @SendTo("/topic/group-messages/{groupId}/task")
+    public TaskWsNotificationDto modifyTask(TaskWsNotificationDto notificationVo, @DestinationVariable("groupId") int groupId, @DestinationVariable("taskId") int taskId){
+        return notificationVo;
     }
 }
